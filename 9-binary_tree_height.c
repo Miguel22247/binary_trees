4b543e6b780c645ee8e4a134957d383106462cb7
Miguel22247 <minipachru@gmail.com>
2021-07-20 14:54:15 -0300

    added 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -16,9 +16,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		height_left = binary_tree_height(tree->left);
 		height_right = binary_tree_height(tree->right);
 	
-	if (height_right < height_left)
-		return (height_right + 1);
-	else
+	if (height_left > height_right)
 		return (height_left + 1);
-
+	else
+		return (height_right + 1);
 }
