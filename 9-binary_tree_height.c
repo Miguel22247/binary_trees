39f3b24f41df25ec9ffb3668937e06e1ccdd509d
Miguel22247 <minipachru@gmail.com>
2021-07-20 13:10:30 -0300

    added 9

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+/**
+* binary_tree_height - Returns the height of the binary tree
+* @tree: the binary tree
+* Returns: the height of the binary tree
+*/
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t height_left = 0, height_right = 0;
+
+	if (!tree)
+		return (0);
+	
+	if (!tree->left && !tree->right)
+		height_left = binary_tree_height(tree->left);
+		height_right = binary_tree_height(tree->right);
+	
+	if (height_right < height_left)
+		return (height_right + 1);
+	else
+		return (height_left + 1);
+
+}
