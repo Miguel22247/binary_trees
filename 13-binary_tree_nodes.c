aa8f6f56250ff93763ed808bdc7c8b71742c90b4
Miguel22247 <minipachru@gmail.com>
2021-07-21 11:03:01 -0300

    fixed checker i guess

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -8,7 +8,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	size_t node_count = 0;
 
-	if (!tree)
+	if (!tree || !tree->left || !tree->right)
 		return (0);
 
 	if (tree->left || tree->right)
