9131b764fa5c68a386e0a459d2f2dddf82ade866
Miguel22247 <minipachru@gmail.com>
2021-07-21 10:28:06 -0300

    improved code in 7 and 8

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -11,11 +11,11 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 	if (!tree || !func)
 		return;
 
-	if (tree->left != NULL)
+	if (tree->left)
 		binary_tree_inorder(tree->left, func);
 
 	func(tree->n);
 
-	if (tree->right != NULL)
+	if (tree->right)
 		binary_tree_inorder(tree->right, func);
 }
--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -7,13 +7,13 @@
 
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree == NULL)
+	if (!tree || !func)
 		return;
 
-	if (tree->left != NULL)
+	if (tree->left)
 		binary_tree_postorder(tree->left, func);
 
-	if (tree->right != NULL)
+	if (tree->right)
 		binary_tree_postorder(tree->right, func);
 
 	func(tree->n);
