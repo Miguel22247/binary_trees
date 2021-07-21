8ec7405dfdc318002c7f1368d332a37015428c6e
Miguel22247 <minipachru@gmail.com>
2021-07-21 10:58:42 -0300

    added 13

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+/**
+* binary_tree_nodes - counts the nodes with at least 1 child
+* @tree: is the binary tree
+* Return: the number of nodes
+*/
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t node_count = 0;
+	if (!tree)
+		return (0);
+
+	if (tree->left || tree->right)
+		node_count++;
+	else
+		node_count = node_count + binary_tree_nodes(tree->left);
+		node_count = node_count + binary_tree_nodes(tree->right);
+		return (node_count);
+}
