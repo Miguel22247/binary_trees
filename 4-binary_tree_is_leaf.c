8c8a864221debb130d482c8e02260d313f19c712
Miguel22247 <minipachru@gmail.com>
2021-07-21 09:59:06 -0300

    fixed betty

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
-* binary_tree_is leaf - a function that deletes an entire binary tree
+* binary_tree_is_leaf - a function that deletes an entire binary tree
 * @node: is a pointer to the node to be checked
 * Return: return 1 or 0
 */
