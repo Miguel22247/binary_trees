f06940abbb4265cbacf8a86d6f61cc4054bffc92
Miguel22247 <minipachru@gmail.com>
2021-07-21 15:19:36 -0300

    fixed betty

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -23,7 +23,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	binary_tree_t *uncle;
-	
+
 	if (!node || !node->parent)
 		return (NULL);
 	uncle = binary_tree_sibling(node->parent);
