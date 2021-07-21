f260eb95ebff9c0679e41d8a98ff8cbbd6de8984
Miguel22247 <minipachru@gmail.com>
2021-07-21 09:41:36 -0300

    fixed betty

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -2,7 +2,7 @@
 /**
 * binary_tree_height - Returns the height of the binary tree
 * @tree: the binary tree
-* Returns: the height of the binary tree
+* Return: the height of the binary tree
 */
 
 size_t binary_tree_height(const binary_tree_t *tree)
@@ -11,11 +11,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
-	
+
 	if (!tree->left && !tree->right)
 		height_left = binary_tree_height(tree->left);
 		height_right = binary_tree_height(tree->right);
-	
+
 	if (height_left > height_right)
 		return (height_left + 1);
 	else
