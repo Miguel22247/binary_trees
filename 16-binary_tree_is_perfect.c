ed3c165784a69b7f0b0be64e7d7fff2cefcf7997
Miguel22247 <minipachru@gmail.com>
2021-07-21 12:36:08 -0300

    added another check

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -41,6 +41,10 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (1);
 
 	if (binary_tree_height(tree->left) == binary_tree_height(tree->right))
-		return (1);
+	{
+		if (binary_tree_is_perfect(tree->right) &&
+			binary_tree_is_perfect(tree->left))
+				return (1);
+	}
 	return (0);
 }
