f0a9ada6508abfbbccf5c274941aa9d258b09438
Miguel22247 <minipachru@gmail.com>
2021-07-21 13:01:24 -0300

    fixed betty

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -7,9 +7,9 @@
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (!node || !node->parent)
-		return NULL;
+		return (NULL);
 	if (node->parent->right == node)
-		return(node->parent->left);
+		return (node->parent->left);
 	else
-		return(node->parent->right);
+		return (node->parent->right);
 }
