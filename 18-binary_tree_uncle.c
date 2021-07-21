792c4cd173fb81249e9f54db10c372124c622d92
Miguel22247 <minipachru@gmail.com>
2021-07-21 14:52:41 -0300

    added 18

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+/**
+* binary_tree_uncle - Finds the uncle of a node
+* @node: the uncle node
+* Return: the uncle node
+*/
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (!node)
+		return (NULL);
+}
