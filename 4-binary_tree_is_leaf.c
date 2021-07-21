ef120efa3fbc81ba0fde5ace62eb1d0ef24c7abc
Miguel22247 <minipachru@gmail.com>
2021-07-21 10:13:48 -0300

    changed or for and

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -7,9 +7,9 @@
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node == NULL)
+	if (!node)
 		return (0);
-	else if (node->left == NULL || node->right == NULL)
+	else if (!node->left && !node->right)
 		return (1);
 	else
 		return (0);
