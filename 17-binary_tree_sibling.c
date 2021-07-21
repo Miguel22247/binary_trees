faa380a496185a467e67bff293421af6f05cad1b
Miguel22247 <minipachru@gmail.com>
2021-07-21 12:57:44 -0300

    reduced code of 17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -8,29 +8,8 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (!node || !node->parent)
 		return NULL;
-	if (node->parent->left == node)
-	{
-		if (node->parent->right)
-		{
-			return (node->parent->right);
-		}
-		else
-		{
-			return (NULL);
-		}	
-	}
+	if (node->parent->right == node)
+		return(node->parent->left);
 	else
-	{
-		if (node->parent->left)
-		{
-			if (node->parent->left == node)
-			{
-				return (node->parent->left);
-			}
-			else
-			{
-				return (NULL);
-			}
-		}
-	}
-}
+		return(node->parent->right);
+
